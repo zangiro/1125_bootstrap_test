69dad863044b7bde4217497c2189a1b87b713ff3
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-05-15 20:48:38 +0900

    SCSSの記述位置テスト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_search
+
+  private
+
+  def set_search
+    @q = User.ransack(params[:q])
+  end
 end
