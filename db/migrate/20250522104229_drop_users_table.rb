77a0cda6194fb86bb93329939544a901f97d43b5
ヨシアキ <yoshi@yoshiakis-MacBook-Air.local>
2025-05-22 19:44:10 +0900

    admin検証のためuserテーブル削除。適用

---
+++ db/migrate/20250522104229_drop_users_table.rb
@@ -0,0 +1,5 @@
+class DropUsersTable < ActiveRecord::Migration[7.2]
+  def change
+    drop_table :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_13_104941) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_22_104229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,13 +47,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_13_104941) do
     t.text "address"
     t.float "latitude"
     t.float "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
